aa25a00378199cd3b981f4cb047a0d6661047687
Guillermo Sanchez <49462669+guille2400@users.noreply.github.com>
2019-10-29 16:40:49 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
-#include <iostream>
+#include "Biblios.h"
 #include "Driver.h"
 #include "Table.h"
 #include "Token_Stream.h"
 
 int main() {
+    int no_of_errors;
     table["pi"] = 3.1415926535897932385;
     table["e"] = 2.7182818284590452354; // insert predefined names
     calculate();
-    return no_of_Errors;
+    return no_of_errors;
     return 0;
 }
